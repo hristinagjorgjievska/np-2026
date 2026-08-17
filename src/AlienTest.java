a6438b2caed5fb7ea3f72b103c3e16330c5f37e4
hristinagjorgjievska <hristinna791@gmail.com>
2026-08-17 14:52:54 +0200

    initial commit

---
+++ src/AlienTest.java
@@ -0,0 +1,103 @@
+class Alien {
+//    public static final int SNAKE_ALIEN = 0;
+//    public static final int OGRE_ALIEN = 1;
+//    public static final int MARSHMALLOW_MAN_ALIEN = 2;
+//    public int type; // Stores one of the three above types
+    private int health;
+    private String name;
+
+    public Alien(int health, String name) {
+        this.health = health;
+        this.name = name;
+    }
+
+    public int getDamage(){
+        return 0;
+    }
+}
+
+class SnakeAlien extends Alien{
+
+    public SnakeAlien(int health, String name) {
+        super(health, name);
+    }
+
+    @Override
+    public int getDamage(){
+        return 10;
+    }
+}
+
+class OgreAlien extends Alien{
+
+    public OgreAlien(int health, String name) {
+        super(health, name);
+    }
+
+    @Override
+    public int getDamage(){
+        return 6;
+    }
+}
+
+class MarshmallowManAlien extends Alien{
+
+    public MarshmallowManAlien(int health, String name) {
+        super(health, name);
+    }
+
+    @Override
+    public int getDamage(){
+        return 1;
+    }
+}
+
+class AlienPack {
+    private Alien[] aliens;
+
+    public AlienPack(int numAliens) {
+        aliens = new Alien[numAliens];
+    }
+
+    public void addAlien(Alien newAlien, int index) {
+        aliens[index] = newAlien;
+    }
+
+    public Alien[] getAliens() {
+        return aliens;
+    }
+
+//    public int calculateDamage() {
+//        int damage = 0;
+//        for (int i = 0; i < aliens.length; i++) {
+//            if (aliens[i].type == Alien.SNAKE_ALIEN) {
+//                damage += 10;// Snake does 10 damage
+//            } else if (aliens[i].type == Alien.OGRE_ALIEN) {
+//                damage += 6;// Ogre does 6 damage
+//            } else if (aliens[i].type == Alien.MARSHMALLOW_MAN_ALIEN) {
+//                damage += 1;
+//                // Marshmallow Man does 1 damage
+//            }
+//        }
+//        return damage;
+//    }
+
+    public int calculateDamage() {
+        int damage = 0;
+        for (int i = 0; i < aliens.length; i++) {
+            damage += aliens[i].getDamage();
+        }
+        return damage;
+    }
+}
+
+public class AlienTest {
+    public static void main(String[] args) {
+        AlienPack pack = new AlienPack(3);
+        pack.addAlien(new SnakeAlien(100, "Ognen"), 0);
+        pack.addAlien(new OgreAlien(100, "Evica"), 1);
+        pack.addAlien(new MarshmallowManAlien(100, "Kiki"), 2);
+
+        System.out.println("Damage: " + pack.calculateDamage());
+    }
+}
---
+++ src/BankTest.java
@@ -0,0 +1,113 @@
+abstract class Account{
+    private String holderName;
+    private int accountNumber;
+    private double balance;
+
+    public Account(String holderName, int accountNumber, int balance) {
+        this.holderName = holderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    //getBalance() е public за да може контролирано да се чита состојбата од надвор
+    //(пример од Bank), а balance е private за да не може никој да ја менува директно
+    //и неконтролирано, секоја промена мора да помине низ addMoney / withdrawMoney
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void addMoney(double money){
+        balance += money;
+    }
+
+    public void withdrawMoney(double money){
+        balance -= money;
+    }
+}
+
+interface InterestBearingAccount{
+    void addInterest();
+}
+
+class Bank{
+    private Account accounts[];
+    private int numberAccounts;
+
+    public Bank(int n) {
+        accounts = new Account[n];
+        numberAccounts = 0;
+    }
+
+    public void addAccount(Account account){
+        accounts[numberAccounts] = account;
+        numberAccounts += 1;
+    }
+
+    public double totalAssets(){
+        double sum = 0;
+        for (int i = 0; i < numberAccounts; i++){
+            sum += accounts[i].getBalance();
+        }
+
+        return sum;
+    }
+
+    public void addInterest(){
+        for (int i = 0; i < numberAccounts; i++) {
+            if (accounts[i] instanceof InterestBearingAccount){
+                InterestBearingAccount iba = (InterestBearingAccount) accounts[i];
+                iba.addInterest();
+            }
+        }
+    }
+}
+
+class InterestCheckingAccount extends Account implements InterestBearingAccount{
+
+    public static final double interest = 0.03;
+
+    public InterestCheckingAccount(String holderName, int accountNumber, int balance) {
+        super(holderName, accountNumber, balance);
+    }
+
+    @Override
+    public void addInterest() {
+        double amount = getBalance() * interest;
+        addMoney(amount);
+    }
+}
+
+class PlatinumCheckingAccount extends InterestCheckingAccount {
+
+    public PlatinumCheckingAccount(String holderName, int accountNumber, int balance) {
+        super(holderName, accountNumber, balance);
+    }
+
+    @Override
+    public void addInterest() {
+        double amount = getBalance() * (interest * 2);
+        addMoney(amount);
+    }
+}
+
+class NonInterestCheckingAccount extends Account{
+
+    public NonInterestCheckingAccount(String holderName, int accountNumber, int balance) {
+        super(holderName, accountNumber, balance);
+    }
+}
+
+public class BankTest {
+    public static void main(String[] args) {
+        Bank bank = new Bank(10);
+
+        bank.addAccount(new NonInterestCheckingAccount("Kiki", 1, 1000));
+        bank.addAccount(new InterestCheckingAccount("Evica", 2, 1000));
+        bank.addAccount(new PlatinumCheckingAccount("Ognen", 3, 1000));
+
+        System.out.println("Before interest: " + bank.totalAssets());
+        bank.addInterest();
+        System.out.println("After interest: " + bank.totalAssets());
+    }
+}
